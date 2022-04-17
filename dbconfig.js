9a2df575bb42572ff3d429634699dbc3329169fa
khalil <khilel208@gmail.com>
2022-04-17 14:48:39 +0300

    17-4-2022
    cc

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+    user :'sa',
+    password :'sa123456789$',
+    server:'localhost',
+    database:'Products',
+    options:{
+        trustedconnection: true,
+        enableArithAbort : true, 
+        instancename :''
+    },
+    port : 1433
+}
+
+module.exports = config; 
